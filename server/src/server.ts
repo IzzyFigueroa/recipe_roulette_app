84c8a612f748ccfa30cbef6454da547215cc7a9e
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-11-08 17:50:39 -0500

    ready for deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use('/', routes);
 
-await client.sync({ force: true });
+await client.sync({ force: false });
 
 // Render related code for deployment
 if (process.env.PORT) {
